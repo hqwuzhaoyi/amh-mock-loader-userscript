be1699978f981a9870f48e095aa888eba2100018
Prajna <hqwuzhaoyi@gmail.com>
2025-11-18 11:39:09 +0800

    添加 AMH Mock Loader 用户脚本以自动加载本地 mock.js

---
+++ amh-mock-loader.user.js
@@ -0,0 +1,31 @@
+// ==UserScript==
+// @name         AMH Mock Loader
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  在 AMH 内网页面自动加载本地 mock.js
+// @author       you
+// @match        https://web.amh-group.com:8080/*
+// @run-at       document-start
+// @grant        none
+// ==/UserScript==
+
+(function () {
+    'use strict';
+
+    function injectMock() {
+        const script = document.createElement('script');
+        script.src = 'https://localhost:3366/mock.js';
+        script.type = 'text/javascript';
+        script.async = false; // 尽量保证按顺序执行
+
+        // 有 head 就塞 head，没有就塞到 html 根上
+        (document.head || document.documentElement).appendChild(script);
+    }
+
+    if (document.readyState === 'loading') {
+        // 尽早插入，等 DOM 就绪再挂 script
+        document.addEventListener('DOMContentLoaded', injectMock, { once: true });
+    } else {
+        injectMock();
+    }
+})();
